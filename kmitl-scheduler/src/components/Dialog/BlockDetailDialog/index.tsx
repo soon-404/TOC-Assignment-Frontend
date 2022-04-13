50e648889d206c0728f280a121b643c2001f084b
Dear <dear19052543@gmail.com>
2022-04-14 01:41:10 +0700

    dialog | add or remove course

--- kmitl-scheduler/src/components/Dialog/BlockDetailDialog/index.tsx
+++ kmitl-scheduler/src/components/Dialog/BlockDetailDialog/index.tsx
@@ -1,8 +1,9 @@
 import { Box, Button, IconButton, Typography } from '@mui/material'
 import ContentCopyIcon from '@mui/icons-material/ContentCopy'
-import { Course } from 'types'
+import { Course, DateRange } from 'types'
 import { useStore } from 'hooks/useStore'
-
+import AlarmAddIcon from '@mui/icons-material/AlarmAdd'
+import AutoDeleteIcon from '@mui/icons-material/AutoDelete'
 interface LineInfoProps {
   key: string
   label: string
@@ -31,6 +32,39 @@ const LineInfo = ({ key, label, values, isCopyable }: LineInfoProps) => {
   )
 }
 
+interface DateInfoProps {
+  type: 'midterm' | 'final'
+  values: string | DateRange
+}
+interface DateInfoLabel {
+  midterm: string
+  final: string
+}
+
+const DateInfo = ({ type, values }: DateInfoProps) => {
+  const label: DateInfoLabel = {
+    midterm: 'วันสอบกลางภาค',
+    final: 'วันสอบปลายภาค',
+  }
+  // console.log(type, values)
+  return (
+    <Box display="flex" alignItems="center">
+      <Typography variant="body1">{label[type as keyof typeof label]} :</Typography>
+      <Typography variant="body2">
+        {typeof values === 'string'
+          ? values
+          : new Date(values.start * 1000).getHours() +
+            ':' +
+            new Date(values.start * 1000).getMinutes() +
+            ' - ' +
+            new Date(values.end * 1000).getHours() +
+            ':' +
+            new Date(values.end * 1000).getMinutes()}
+      </Typography>
+    </Box>
+  )
+}
+
 type BlockDetailPickOut = Omit<Course, 'midterm' | 'final' | 'section'>
 type KeyOfBlockDetailPickOut = keyof BlockDetailPickOut
 type CopyableKey = Extract<KeyOfBlockDetailPickOut, 'id' | 'name'>
@@ -42,18 +76,33 @@ interface BlockDetailDialogProps {
 const CopyableKey: string[] = ['id', 'name'] as CopyableKey[]
 
 export const BlockDetailDialog = ({ course }: BlockDetailDialogProps) => {
-  const { setSelectedCourses } = useStore()
+  const { freeCourses, setFreeCourses, selectedCourses, setSelectedCourses } = useStore()
 
   const filteredCourseKey: BlockDetailPickOut = course
+
+  const isSelected = (courseId: string) => {
+    const target = selectedCourses.find((course) => course.id == courseId)
+    return target ? true : false
+  }
+
+  function editSchedule() {
+    if (isSelected(course.id)) {
+      const leftedBlocks = selectedCourses.filter((selectedCourse) => selectedCourse.id !== course.id)
+      setSelectedCourses([...leftedBlocks])
+      setFreeCourses((prev) => [...prev, course])
+    } else {
+      const leftedBlocks = freeCourses.filter((freeCourse) => freeCourse.id !== course.id)
+      setFreeCourses([...leftedBlocks])
+      setSelectedCourses((prev) => [...prev, course])
+    }
+  }
 
   return (
     <Box>
       {/* head */}
       <Typography variant="h6">{course.name}</Typography>
-
       {/* Table */}
       {/* use data in course.section */}
-
       {/* footer */}
       {/* caution bug at onClick */}
       {/* {Object.keys(filteredCourseKey).map((key: string) => (
@@ -64,8 +113,23 @@ export const BlockDetailDialog = ({ course }: BlockDetailDialogProps) => {
           isCopyable={CopyableKey.includes(key)}
         />
       ))} */}
-      <Button variant="contained" onClick={() => setSelectedCourses((prev) => [...prev, course])}>
-        add to schedule
+      <LineInfo
+        key={'teacher'}
+        label={'อาจารย์ผู้สอน'}
+        values={filteredCourseKey['teacher'].length ? filteredCourseKey['teacher'] : ['ยังไม่ประกาศ']}
+        isCopyable={false}
+      />
+
+      <DateInfo type={'midterm'} values={course.midterm || 'ยังไม่ประกาศ'} />
+      <DateInfo type={'final'} values={course.final || 'ยังไม่ประกาศ'} />
+
+      <Button
+        variant={isSelected(course.id) ? 'outlined' : 'contained'}
+        startIcon={isSelected(course.id) ? <AutoDeleteIcon /> : <AlarmAddIcon />}
+        onClick={editSchedule}
+        sx={{ float: 'right', marginTop: 2 }}
+      >
+        {isSelected(course.id) ? 'remove from schedule' : 'add to schedule'}
       </Button>
     </Box>
   )
